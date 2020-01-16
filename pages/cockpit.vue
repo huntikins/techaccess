702e89c879a5682b190582b17015b6f0dd1a1dcd
Hunter Trammell <huntertrammell@Digital-Branch-MacBook-Pro.local>
2020-01-16 09:45:34 -0600

    added iframe to url path for easier access of cockpit cms

---
+++ pages/cockpit.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="iframe-container">
+    <iframe src="http://localhost:8080/auth/login" allowfullscreen></iframe>
+  </div>
+</template>
+<script>
+export default {
+  layout: "cockpit"
+};
+</script>
+<style lang="css" scoped>
+.container {
+  margin: 0 !important;
+  padding: 0 !important;
+}
+.iframe-container {
+  overflow: hidden;
+  padding-top: 56.25%;
+  position: relative;
+}
+
+.iframe-container iframe {
+  border: 0;
+  height: 100%;
+  left: 0;
+  position: absolute;
+  top: 0;
+  width: 100%;
+}
+
+/* 4x3 Aspect Ratio */
+.iframe-container-4x3 {
+  padding-top: 75%;
+}
+</style>
