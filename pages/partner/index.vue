d0d3feccd4190d08baf1f2a0b880d6fbfb1af399
Hunter Trammell <huntertrammell@Digital-Branch-MacBook-Pro.local>
2020-01-16 14:46:34 -0600

    added mailchimp signup form

--- pages/partner/index.vue
+++ pages/partner/index.vue
@@ -8,6 +8,63 @@
           style="word-break: normal;"
         ></h1>
       </v-col>
+      <v-col :md="12" :sm="12">
+        <!-- Begin Mailchimp Signup Form -->
+        <link
+          href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css"
+          rel="stylesheet"
+          type="text/css"
+        />
+        <div id="mc_embed_signup">
+          <form
+            action="https://kclibrary.us18.list-manage.com/subscribe/post?u=02676a46f25558c3728470954&amp;id=54e93eb8b1"
+            method="post"
+            id="mc-embedded-subscribe-form"
+            name="mc-embedded-subscribe-form"
+            class="validate"
+            target="_blank"
+            novalidate
+          >
+            <div id="mc_embed_signup_scroll">
+              <label for="mce-EMAIL">Subscribe to our Partner Newsletter</label>
+              <input
+                type="email"
+                value=""
+                name="EMAIL"
+                class="email"
+                id="mce-EMAIL"
+                placeholder="email address"
+                required
+              />
+              <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+              <div
+                style="position: absolute; left: -5000px;"
+                aria-hidden="true"
+              >
+                <input
+                  type="text"
+                  name="b_02676a46f25558c3728470954_54e93eb8b1"
+                  tabindex="-1"
+                  value=""
+                />
+              </div>
+              <div class="clear">
+                <v-btn
+                  type="submit"
+                  color="secondary"
+                  value="Subscribe"
+                  name="subscribe"
+                  id="mc-embedded-subscribe"
+                  class="button"
+                  >Subscribe</v-btn
+                >
+              </div>
+            </div>
+          </form>
+        </div>
+
+        <!--End mc_embed_signup-->
+      </v-col>
     </v-row>
     <v-row>
       <template v-for="section in sections">
@@ -31,8 +88,15 @@
 <script>
 export default {
   data: () => ({
-    alert: true
+    alert: true,
+    dialog: false
   }),
+  methods: {
+    submit() {
+      this.dialog = false;
+      this.$v.$touch();
+    }
+  },
   async asyncData({ app }) {
     const { data } = await app.$axios.post(
       process.env.PARTNERPAGE,
