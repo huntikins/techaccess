a3469b3de5f157050dd93252e6e039d3f761c92c
Hunter Trammell <hunterttrammell@gmail.com>
2020-01-23 09:28:46 -0600

    updated nuxt-link

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,20 +8,14 @@
       app
     >
       <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          nuxt
-          exact
-        >
+        <nuxt-link v-for="(item, i) in items" :key="i" :to="item.to" exact>
           <v-list-item-action>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-action>
           <v-list-item-content>
             <v-list-item-title v-text="item.title" />
           </v-list-item-content>
-        </v-list-item>
+        </nuxt-link>
         <v-list-item href="https://www.kclibrary.org/">
           <v-list-item-action>
             <v-icon>mdi-library</v-icon>
