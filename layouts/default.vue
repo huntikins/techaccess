c0d5cf2f791d8a55210d7539c89091a5739f9d2b
Hunter Trammell <hunterttrammell@gmail.com>
2020-01-23 10:04:07 -0600

    updated links in navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,14 +8,20 @@
       app
     >
       <v-list>
-        <nuxt-link v-for="(item, i) in items" :key="i" :to="item.to" exact>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          nuxt
+          exact
+        >
           <v-list-item-action>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-action>
           <v-list-item-content>
             <v-list-item-title v-text="item.title" />
           </v-list-item-content>
-        </nuxt-link>
+        </v-list-item>
         <v-list-item href="https://www.kclibrary.org/">
           <v-list-item-action>
             <v-icon>mdi-library</v-icon>
