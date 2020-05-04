8aa1d5cdfb41a0edb370306b68df518ec640d82c
Hunter Trammell <hunterttrammell@gmail.com>
2020-05-04 14:14:13 -0500

    removed semi-colons on column width to see if that was the issue. Not sure why columns are stacking and not floating

--- pages/learn/index.vue
+++ pages/learn/index.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="pa-5">
     <v-row>
-      <v-col :md="12" :sm="12">
+      <v-col md="12" sm="12">
         <v-card class="pa-4 mt-4" flat>
           <h1 class="text-center">Create Your Own Path</h1>
-          <v-col :md="4" :sm="12">
+          <v-col md="4" sm="12">
             <v-card class="mx-auto" max-width="344" to="/appointment" hover>
               <v-img
                 src="https://cms.kclibrary.org/storage/uploads/2020/02/12/5e44696768dfe3.png"
@@ -12,7 +12,7 @@
               ></v-img>
             </v-card>
           </v-col>
-          <v-col :md="4" :sm="12">
+          <v-col md="4" sm="12">
             <v-card
               class="mx-auto mt-5"
               max-width="344"
@@ -25,7 +25,7 @@
               ></v-img>
             </v-card>
           </v-col>
-          <v-col :md="4" :sm="12">
+          <v-col md="4" sm="12">
             <v-card
               class="mx-auto mt-5"
               max-width="344"
@@ -42,11 +42,11 @@
       </v-col>
     </v-row>
     <v-row>
-      <v-col :md="12" :sm="12">
+      <v-col md="12" sm="12">
         <v-card class="pa-4 mt-4" flat>
           <h1 class="text-center">Let Us Help You</h1>
           <template v-for="resource in resources">
-            <v-col :md="4" :sm="12" :key="resource._id">
+            <v-col md="4" sm="12" :key="resource._id">
               <v-card class="mx-auto" max-width="344" hover>
                 <v-img
                   :src="img_base_url + resource.image.path"
