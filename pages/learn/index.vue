014c7c421d63cc4bf730a7101f017b1e91a55a0a
Hunter Trammell <hunterttrammell@gmail.com>
2020-07-10 13:50:52 -0500

    update content

--- pages/learn/index.vue
+++ pages/learn/index.vue
@@ -5,7 +5,7 @@
         <v-card class="pa-4 mt-4" flat>
           <h1 class="text-center">Create Your Own Path</h1>
           <v-row>
-            <v-col :md="4" :sm="12">
+            <v-col :md="3" :sm="12">
               <v-card
                 class="mx-auto mt-5"
                 max-width="344"
@@ -18,7 +18,7 @@
                 ></v-img>
               </v-card>
             </v-col>
-            <v-col :md="4" :sm="12">
+            <v-col :md="3" :sm="12">
               <v-card
                 class="mx-auto mt-5"
                 max-width="344"
@@ -31,15 +31,27 @@
                 ></v-img>
               </v-card>
             </v-col>
-            <v-col :md="4" :sm="12">
+            <v-col :md="3" :sm="12">
               <v-card
                 class="mx-auto mt-5"
                 max-width="344"
-                href="https://www.projectoutcome.org/responses/18137"
+                href="https://www.kclibrary.org/events-activities-calendar/month?combine=&field_event_category_target_id=3523&field_location_target_id=All"
                 hover
               >
                 <v-img
-                  src="https://cms.kclibrary.org/storage/uploads/2020/01/29/5e3204aeb8510TechCoachFeedback.png"
+                  src="https://cms.kclibrary.org/storage/uploads/2020/02/12/5e4469671dfcb2.png"
+                  contain
+                ></v-img>
+              </v-card>
+            <v-col :md="3" :sm="12">
+              <v-card
+                class="mx-auto mt-5"
+                max-width="344"
+                href="https://kclibrary.digitallearn.org"
+                hover
+              >
+                <v-img
+                  src="https://cms.kclibrary.org/storage/uploads/2020/01/29/5e31e35feb4eeonline-tutorials.png"
                   contain
                 ></v-img>
               </v-card>
@@ -54,7 +66,7 @@
           <h1 class="text-center">Let Us Help You</h1>
           <v-row>
             <template v-for="resource in resources">
-              <v-col :md="4" :sm="12" :key="resource._id">
+              <v-col :md="3" :sm="12" :key="resource._id">
                 <v-card class="mx-auto mt-5" max-width="344" hover>
                   <v-img
                     :src="img_base_url + resource.image.path"
